869f33b3668e66310d48b6b60f22e975a279c689
zokevlar98 <zqouri11@gmail.com>
2024-11-22 22:39:21 +0100

    done Fri Nov 22 22:39:21 +01 2024

--- cpp00/ex00/main.cpp
+++ cpp00/ex00/main.cpp
@@ -2,6 +2,15 @@
 
 int	main()
 {
-    std::cout<<"Hello world!"<<std::endl;
-    return (0);
+	long x;
+	int y;
+
+	std::cout<<"Enter tow number sperate with space: ";
+	std::cin>> x >> y;
+	std::cout<<"You numbers is : "<< x << " and " << y <<std::endl;
+	// std::cout<<"number * 2 : "<<x * 2 <<std::endl;
+	#if 0
+		std::cout<<"hello world"<<std::endl;
+	#endif
+	return (0);
 }
