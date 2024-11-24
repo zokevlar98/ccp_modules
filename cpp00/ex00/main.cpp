de39579bbc8ce49efdf65013de4078bf2cf9738c
zokevlar98 <zqouri11@gmail.com>
2024-11-24 18:05:35 +0100

    done Sun Nov 24 18:05:35 +01 2024

--- cpp00/ex00/main.cpp
+++ cpp00/ex00/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include <limits>
+#include <iomanip> // std::setprecision()
+
 
 int	main()
 {
-	long x;
-	int y;
+	long	x;
+	int		y;
 
 	std::cout<<"Enter tow number sperate with space: ";
 	std::cin>> x >> y;
 	std::cout<<"You numbers is : "<< x << " and " << y <<std::endl;
-	// std::cout<<"number * 2 : "<<x * 2 <<std::endl;
+	std::cout<<"number * 2 : "<<x * 2 <<std::endl;
+	std::cout << 6.7f << '\n';
+	std::cout<<std::setprecision(17);
+	std::cout << 3.33333333333333333333333333333333333333f << std::endl; // f suffix means float
+    std::cout << 3.3333333333333333333333333333333333333 << std::endl; // no suffix means double
+	static_cast<int>(x);//Explicit type conversion
 	#if 0
-		std::cout<<"hello world"<<std::endl;
+		// std::cout<<"hello world"<<std::endl;
+		std::cout << std::numeric_limits<float>::is_iec559 << '\n';
+    	std::cout << std::numeric_limits<double>::is_iec559 << '\n';
+    	std::cout << std::numeric_limits<long double>::is_iec559 << '\n';
 	#endif
 	return (0);
 }
---
+++ cpp00/ex00/main.hpp
@@ -0,0 +1,12 @@
+#ifdef MAIN_H
+# define MAIN_H
+
+
+class Sample
+{
+    public :
+        Sample( void );
+        ~Sample( void );
+};
+
+#endif
